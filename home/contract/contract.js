8346859cd656233059260ac672a14ddfe258703d
彭黎明 <295864237@qq.com>
2017-09-25 14:27:01 +0800

    925

--- home/contract/contract.js
+++ home/contract/contract.js
@@ -16,7 +16,6 @@ import {
     Platform,
 } from 'react-native';
 import { StackNavigator,TabNavigator } from "react-navigation";
-
 import config from '../../common/config';
 import request from '../../common/request';
 import toast from '../../common/toast';
@@ -25,22 +24,17 @@ import Header from '../../common/header';
 const screenW = Dimensions.get('window').width;
 export default class Contract  extends Component {
 
-
     constructor(props) {
         super(props);
         this.state = {
             contractdata:[],
             load:false,
-
+            load1:false,
         };
     }
-
-
     //耗时操作放在这里面
     componentDidMount(){
-
         this.subscription = DeviceEventEmitter.addListener('user_id',(value) => {
-
             var url = config.api.base + config.api.select_contract;
             request.post(url,{
                 user_id: value,//登录者id
@@ -49,81 +43,69 @@ export default class Contract  extends Component {
                 if(responseText.sing==1){
                     this.setState({
                         load: true,
+                        load1: true,
                         contractdata:responseText.data,
                     })
                 }else{
-
-                    toast.center('没有数据');
+                    this.setState({
+                        load1: true,
+                    })
                 }
             }).catch((error)=>{
                 toast.bottom('网络连接失败，请检查网络后重试');
             })
         })
-
         this.getNet();
-
     }
 
     getNet(){
-
         var url = config.api.base + config.api.select_contract;
         request.post(url,{
             user_id: this.props.navigation.state.params.user_id,//人员id
         }).then((responseText) => {
-
             if(responseText.sing==1){
                 this.setState({
                     load: true,
+                    load1: true,
                     contractdata:responseText.data,
                 })
             }else{
-
-                toast.center('没有数据');
+                this.setState({
+                    load1: true,
+                })
             }
-
-
         }).catch((error)=>{
             toast.bottom('网络连接失败，请检查网络后重试');
         })
-
     }
-
-
-
-
 
     OpBack() {
         this.props.navigation.goBack(null)
     }
-
     newBulidContract() {
         this.props.navigation.navigate('NewBulidContract',{user_id:this.props.navigation.state.params.user_id
             ,company_id:this.props.navigation.state.params.company_id
         })
     }
-
-
      //合同详情
     contract_detail(e){
         this.props.navigation.navigate('contract_detail',{contract_id:e,company_id:this.props.navigation.state.params.company_id,user_id:this.props.navigation.state.params.user_id})
     }
 
 
-
-
     render() {
+        if(!this.state.load1){
+            return (<Loading/>);
+        }
 
         var contractlist=this.state.contractdata;
         var list=[];
-
-
-        for(var i in contractlist){
-
-            var contract_status;
-
+        if(this.state.load){
+            for(var i in contractlist){
+                var contract_status;
                 if(contractlist[i].status==1){
                     contract_status='待审批';
-               }else if(contractlist[i].status==2){
+                }else if(contractlist[i].status==2){
                     contract_status='审批通过';
                 }
                 else if(contractlist[i].status==3){
@@ -132,54 +114,52 @@ export default class Contract  extends Component {
                 else if(contractlist[i].status==4){
                     contract_status='已撤销';
                 }
+                list.push(
+                    <View key={i}>
+                        <View style={{marginTop:8,marginBottom:5}}>
+                            <TouchableHighlight
+                                underlayColor={'#fefefe'}
+                                onPress={this.contract_detail.bind(this,contractlist[i].id)}>
+                                <View style={[styles.place,styles.borderTop,styles.borderBottom,{height:40,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
+                                    <Text style={{fontSize:14,color:'#333'}}>{contractlist[i].contract_name}</Text>
+                                    <Text>{contract_status}</Text>
+                                </View>
+                            </TouchableHighlight>
 
-            list.push(
-
-                <View key={i}>
-                    <View style={{marginTop:8,marginBottom:5}}>
+                        </View>
                         <TouchableHighlight
                             underlayColor={'#fefefe'}
                             onPress={this.contract_detail.bind(this,contractlist[i].id)}>
-                            <View style={[styles.place,styles.borderTop,styles.borderBottom,{height:40,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
-                                <Text style={{fontSize:14,color:'#333'}}>{contractlist[i].contract_name}</Text>
-                                <Text>{contract_status}</Text>
+                            <View style={[styles.borderTop,styles.borderBottom,{backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
+                                <View style={[styles.place,styles.borderBottom,{height:40,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
+                                    <Text style={{fontSize:14,color:'#333'}}>合同金额</Text>
+                                    <Text style={{color:'#333'}}>{contractlist[i].contract_jine}</Text>
+                                </View>
+                                <View style={[styles.place,{height:30,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
+                                    <Text style={{fontSize:14,color:'#333'}}>客户名称</Text>
+                                    <Text style={{color:'#333'}}>{contractlist[i].cus_name}</Text>
+                                </View>
+                                <View style={[styles.place,{height:30,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
+                                    <Text style={{fontSize:14,color:'#333'}}>签单业务员</Text>
+                                    <Text style={{color:'#333'}}>{contractlist[i].name}</Text>
+                                </View>
+                                <View style={[styles.place,styles.borderTop,{height:36,backgroundColor:'#fff',paddingLeft:15,paddingRight:15,justifyContent:'flex-start'}]}>
+                                    <Text style={{marginRight:15}}>{contractlist[i].name}</Text>
+                                    <Text>{contractlist[i].time}</Text>
+                                </View>
                             </View>
                         </TouchableHighlight>
-
                     </View>
-
-                    <TouchableHighlight
-                        underlayColor={'#fefefe'}
-                        onPress={this.contract_detail.bind(this,contractlist[i].id)}>
-                        <View style={[styles.borderTop,styles.borderBottom,{backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
-                            <View style={[styles.place,styles.borderBottom,{height:40,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
-                                <Text style={{fontSize:14,color:'#333'}}>合同金额</Text>
-                                <Text style={{color:'#333'}}>{contractlist[i].contract_jine}</Text>
-                            </View>
-
-                            <View style={[styles.place,{height:30,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
-                                <Text style={{fontSize:14,color:'#333'}}>客户名称</Text>
-                                <Text style={{color:'#333'}}>{contractlist[i].cus_name}</Text>
-                            </View>
-                            <View style={[styles.place,{height:30,backgroundColor:'#fff',paddingLeft:15,paddingRight:15}]}>
-                                <Text style={{fontSize:14,color:'#333'}}>签单业务员</Text>
-                                <Text style={{color:'#333'}}>{contractlist[i].name}</Text>
-                            </View>
-                            <View style={[styles.place,styles.borderTop,{height:36,backgroundColor:'#fff',paddingLeft:15,paddingRight:15,justifyContent:'flex-start'}]}>
-                                <Text style={{marginRight:15}}>{contractlist[i].name}</Text>
-                                <Text>{contractlist[i].time}</Text>
-                            </View>
-                        </View>
-                      </TouchableHighlight>
-                   </View>
-
-
-                    )
+                )
+            }
+        }else{
+           list.push(
+               <View style={{marginLeft:150,marginTop:100}}>
+                   <Image  style={{width:48,height:48,marginLeft:15,marginRight:20}}  source={require('../../imgs/customer/empty-content.png')}/>
+                   <Text style={{marginLeft:7}}>暂无数据</Text>
+                </View>
+           )
         }
-
-
-
-
         return (
             <View style={styles.ancestorCon}>
                 <Header title="合同"
